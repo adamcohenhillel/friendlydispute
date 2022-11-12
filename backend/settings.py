d96a8c5827c0a17d30c595a55b787a6775d8cbf2
Adam Cohen Hillel <adamcohenhillel@gmail.com>
2022-11-12 19:40:23 +0000

    redis is working

--- backend/settings.py
+++ backend/settings.py
@@ -5,17 +5,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     """Application settings.
-
-    # these parameters can be configured with environment variables.
     """
-
-    host: str = '127.0.0.1'
-    port: int = 8000
-    workers_count: int = 1  # quantity of workers for uvicorn
-    reload: bool = False  # Enable uvicorn reloading
-    
-    environment: str = 'dev'
-
     # Databases:
     redis_url: str = 'redis://localhost:6379/0'
 
