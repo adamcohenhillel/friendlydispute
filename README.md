683ffb9195ece96723cb4aa799caf565d5e2f65d
Adam Cohen Hillel <adamcohenhillel@gmail.com>
2022-11-12 19:23:38 +0000

    try now

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Friendly Dispute
 Arbitration to solve disputes between friends
+
+
+### TODO:
+1. Enable two parties in one room, securily
+2. Integrate  payement holding (Stripe?)
+3. Frontend (Flutter? might be easy when using something like https://flutterflow.io/)
--- backend/api/routes/dispute.py
+++ backend/api/routes/dispute.py
@@ -1,7 +1,8 @@
 """
 """
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from aioredis import Redis
 
 from core.nlp import query_openai_for_arbitration
 from core.types import Claim
@@ -18,11 +19,12 @@ class DisputeSchema(BaseModel):
 @dispute_router.post('/{room_uuid}')
 async def post(
     body: DisputeSchema,
-    redis: get_redis_connection()
+    redis: Redis = Depends(get_redis_connection)
 ):
     """New arbitration
 
     :param body: request json parameters
     """
-    # request.app.state.redis
+    redis.set('yello', 'bello')
+    redis.get('yello')
     return await query_openai_for_arbitration(claim_1=body.claim_1, claim_2=body.claim_2)
